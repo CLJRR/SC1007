fe43796620e8ebb8fd1ca4d689d77aa3076fa7f0
CLJRR <calvinleejiarong@gmail.com>
2024-03-03 21:21:23 +0800

    h

--- pq.c
+++ pq.c
@@ -312,6 +312,27 @@ int moveMaxToFront(ListNode **ptrHead)
 {
 	if (*ptrHead == NULL)
 		return -1;
+	int max = (*ptrHead)->num;
+	int index = 0;
+	int loc = 0;
+	ListNode *ptr = (*ptrHead)->next;
+	while (ptr != NULL)
+	{
+		index++;
+		ptr = ptr->next;
+		if (ptr->num > max)
+		{
+			max = ptr->num;
+			loc = index;
+		}
+	}
+	if (loc != 0)
+	{
+		removeNode(*ptrHead, loc);
+		insertNode(*ptrHead, 0, max);
+	}
+	return max;
+	return -1;
 }
 
 int concatenate(ListNode **ptrHead1, ListNode *head2)
